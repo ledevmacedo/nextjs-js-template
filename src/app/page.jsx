94361927f9b13383aa3aaeade6952d1bbe072847
Leandro Macedo <leandro.smacedo@Live.com>
2025-10-24 10:34:55 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,6 @@ export default function Home() {
     <div>
       <div className="w-full flex gap-2 h-dvh items-center justify-center">
         <Button>Clique aqui</Button>
-        <Button variant="destructive">Botão vermelho</Button>
         <Button variant="outline">Botão outline</Button>
         <ModeToggle />
       </div>
