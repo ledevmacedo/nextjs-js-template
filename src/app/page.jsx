e6a56456284fd41dd314fd2a4615eeea9e1c9b29
Leandro Macedo <leandro.smacedo@Live.com>
2025-10-24 19:26:39 +0100

    ui: add calendar component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Calendar } from "@/components/ui/calendar";
 
 export default function Home() {
   return (
     <div>
       <div className="w-full flex gap-2 h-dvh items-center justify-center">
-        <Button>Clique aqui</Button>
-        <Button variant="outline">Botão outline</Button>
+        {/* <Button>Clique aqui</Button>
+        <Button variant="outline">Botão outline</Button> */}
         <ModeToggle />
+        <br />
+        <Calendar />
       </div>
     </div>
   );
