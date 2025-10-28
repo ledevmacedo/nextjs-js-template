db78848f83be51f2d7a065d1fb032be9827e71f7
Leandro Macedo <leandro.smacedo@Live.com>
2025-10-28 11:35:29 +0000

    fix: posts route

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -1,12 +1,12 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongodb";
-import { Post } from "@/models/Post";
+import Post from "@/models/Post";
 
 export async function GET(request, { params }) {
   await connectDB();
   const post = await Post.findById(params.id);
 
-  if (!user) {
+  if (!post) {
     return NextResponse.json(
       { error: "Postagem não encontrado" },
       { status: 404 }
