6d18ba82c18e724c91e3222e7c0f90007343abd8
Leandro Macedo <leandro.smacedo@Live.com>
2025-10-28 11:30:58 +0000

    API: posts by id endpoint

---
+++ src/app/api/posts/[id]/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/mongodb";
+import { Post } from "@/models/Post";
+
+export async function GET(request, { params }) {
+  await connectDB();
+  const post = await Post.findById(params.id);
+
+  if (!user) {
+    return NextResponse.json(
+      { error: "Postagem não encontrado" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(post);
+}
